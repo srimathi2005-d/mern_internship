2771ab0f23f009e81a593b2748164fd4c8b5a4a3
srimathi2005-d <srimathidurairaj05@gmail.com>
2025-10-16 16:07:20 +0530

    firstcommit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link} from 'react-router-dom'
+
+
+const Nav = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/" className="nav-link">Home</Link>
+      <Link to="/about" className="nav-link">About</Link>
+      <Link to="/contact" className="nav-link">Contact</Link>
+    </nav>
+  )
+}
+
+export default Nav
